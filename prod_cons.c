a3463d000d5dc40229e01cd4f972aac4be2a7fcd
Hohyeon Cha <outcider112@dankook.ac.kr>
2024-10-14 03:09:34 +0900

    get thread program works just initially

--- prod_cons.c
+++ prod_cons.c
@@ -1,14 +1,17 @@
 #include <pthread.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#define BILLION 1000000000L
 
 typedef struct sharedobject {
 	FILE *rfile;
 	int linenum;
 	char *line;
 	pthread_mutex_t lock;
+	int eof;
 	int full;
 } so_t;
 
@@ -31,9 +34,10 @@ void *producer(void *arg) {
 		}
 		read = getdelim(&line, &len, '\n', rfile);
 		if (read == -1) {
-			so->full = 1;
-			so->line = NULL;
-			pthread_cond_signal(&cond_cv);
+			so->eof = 1;
+			//so->full = 1;
+			//so->line = NULL;
+			pthread_cond_broadcast(&cond_cv);
 			pthread_mutex_unlock(&so->lock);
 			break;
 		}
@@ -59,11 +63,11 @@ void *consumer(void *arg) {
 
 	while (1) {
 		pthread_mutex_lock(&so->lock);
-		while(so->full == 0) {
+		while(so->full == 0 && so->eof == 0) {
 			pthread_cond_wait(&cond_cv, &so->lock);
 		}
 		line = so->line;
-		if (line == NULL) {
+		if (so->full == 0 && so->eof == 1) {
 			pthread_cond_broadcast(&prod_cv);
 			pthread_mutex_unlock(&so->lock);
 			break;
@@ -121,6 +125,10 @@ int main (int argc, char *argv[])
 	share->rfile = rfile;
 	share->line = NULL;
 
+	struct timespec start, end;
+	uint64_t diff;
+	clock_gettime(CLOCK_MONOTONIC, &start);
+
 	pthread_mutex_init(&share->lock, NULL);
 	for (i = 0 ; i < Nprod ; i++)
 		pthread_create(&prod[i], NULL, producer, share);
@@ -136,6 +144,11 @@ int main (int argc, char *argv[])
 		rc = pthread_join(prod[i], (void **) &ret);
 		printf("main: producer_%d joined with %d\n", i, *ret);
 	}
+
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+	printf("Running Threads for %llu ns (%.9f sec)\n", (long long unsigned int) diff, (double) diff / BILLION);
+
 	pthread_exit(NULL);
 
 	exit(0);
